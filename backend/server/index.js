1c98d66b6831fadc26c674521097ca10ba3d3a88
Vaibhav <Vaibhavmathur128@gmail.com>
2024-08-27 14:17:02 +0530

    final deployment

--- backend/server/index.js
+++ backend/server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors({
-  origin: "https://shopease137.vercel.app",
+  origin: "https://frontend-r9mnwb2rs-vaibhav-mathurs-projects.vercel.app/",
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
